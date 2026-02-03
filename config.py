dc2abb68d8c636a1be03c8adfe136bcedd676add
obelchakva <obelchakva@gmail.com>
2026-02-03 12:12:59 +0500

    config have been updeted

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "ваш_токен_бота")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7722825450:AAHKyoLykpV63lmZisNIargwPh5qQXqFlTg")
 BASE_DATA_PATH = "bot_data"
 
 TEACHERS = [
